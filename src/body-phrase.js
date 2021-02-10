287674e733eed6e036c7e43f6305bd4cbb5994e8
Luis <luisprooc@gmail.com>
2021-02-10 12:32:11 -0400

    Data ask from the API and redered

--- src/body-phrase.js
+++ src/body-phrase.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import Box from '@material-ui/core/Box';
-import { Typography,Button } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Typography,Button,Box } from '@material-ui/core';
 import button from './styles/button';
+import author from './styles/author';
 
 const BodyPhrase = () => {
+    const [phrase,savePhrase] = useState({});
+
+    // Ask at the API
+    const seekPhrase = ()=>{
+        fetch("http://localhost:5000/phrase")
+            .then(res => res.json())
+            .then(res => savePhrase(res))
+            .catch(error => console.log(error))
+    }
+
     return(
         <Box component="div" m={1}>
         
             <Typography variant="h6" component="p" align="center">
                 Click button to dropp a random phrase
             </Typography>
-            
-            <Button variant="contained" color="secondary" onClick={()=> console.log("XD")} style={button}>
+
+            <Button variant="contained" color="secondary" style={button} onClick={() => seekPhrase()}>
                 CLICK ME
             </Button>
+
+            {/*When phrase is fill, render it*/}
+
+            {
+                phrase.ID?(
+                    <Box component="blockquote" m={3}>
+                        <Typography variant="subtitle1" component="p" align="justify" >"{phrase.Phrase}"</Typography>
+                        <Typography variant="subtitle2" component="p" align="right" style={author}>-{phrase.Author}</Typography>
+                    </Box>
+                ):null
+            }
 
         </Box>
     );
---
+++ src/styles/author.js
@@ -0,0 +1,6 @@
+const author = {
+    paddingTop: "2rem",
+    fontWeight: "600"
+}
+    
+export default author;
